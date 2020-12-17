680191df0ab4ca503b0576bdff3b0279a7a6d318
nickguerra <nickguerra@gmail.com>
2020-12-17 06:12:16 -0600

    added tests for stat modifiers

---
+++ EvercraftKata.Core/Attribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvercraftKata.Core
+{
+   public struct Attribute
+   {
+      public Attribute(int value)
+      {
+         Value = value;
+      }
+      public int Value { get; set; }
+
+      public int Modifier => (Value - 10) / 2;
+   }
+}
--- EvercraftKata.Tests/AttributeShould.cs
+++ EvercraftKata.Tests/AttributeShould.cs
@@ -1,33 +1,39 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EvercraftKata.Core;
 using Xunit;
 
 namespace EvercraftKata.Tests
 {
    public class AttributeShould
    {
-      [Fact]
-      public void DefaultToTen()
+
+      [Theory]
+      [InlineData(1, -5)]
+      [InlineData(2, -4)]
+      [InlineData(3, -4)]
+      [InlineData(4, -3)]
+      [InlineData(5, -3)]
+      [InlineData(6, -2)]
+      [InlineData(7, -2)]
+      [InlineData(8, -1)]
+      [InlineData(9, -1)]
+      [InlineData(10, 0)]
+      [InlineData(11, 0)]
+      [InlineData(12, 1)]
+      [InlineData(13, 1)]
+      [InlineData(14, 2)]
+      [InlineData(15, 2)]
+      [InlineData(16, 3)]
+      [InlineData(17, 3)]
+      [InlineData(18, 4)]
+      [InlineData(19, 4)]
+      [InlineData(20, 5)]
+      public void HaveSpecificModifierValues(int value, int modifier)
       {
-         var attribute = new Attribute();
+         var attribute = new Attribute(value);
 
-         attribute.Value.Should().Be(10);
-
-      }
-
-      [Fact]
-      public void HaveSpecificModifierValues()
-      {
-
-      }
-
-      public struct Attribute
-      {
-         public int Value { get; set; }
+         attribute.Value.Should().Be(value);
+         attribute.Modifier.Should().Be(modifier);
       }
 
    }
